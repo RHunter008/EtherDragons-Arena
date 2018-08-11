d867780ebfbab2c1d130efcc5ce243ade573b6b3
Dmitriy Belenkiy <4692206+DmitriyBelenkiy@users.noreply.github.com>
2018-08-11 10:50:07 +0300

    Update address-utils.sol

--- utils/address-utils.sol
+++ utils/address-utils.sol
@@ -15,7 +15,7 @@ library AddressUtils {
     {
         uint256 size;
         // XXX Currently there is no better way to check if there is a contract in an address
-        // than to check the size of the code at that address.
+        // than check the code size at that address.
         // See https://ethereum.stackexchange.com/a/14016/36603
         // for more details about how this works.
         // TODO Check this again before the Serenity release, because all addresses will be
